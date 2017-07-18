7accf3aac9d6dc4713feebe0e029baa9ac1f6838
ameniawy <ameniawy18@gmail.com>
2017-07-18 15:39:04 +0200

    binary problem

---
+++ binary825A.py
@@ -0,0 +1,2 @@
+n = int(raw_input())
+print ''.join([str(len(st)) for st in raw_input().split('0')])
--- hongcowNations744A.py
+++ hongcowNations744A.py
@@ -1,4 +1,5 @@
 
+# UNSOLVED 745C or 744A
 n, m, k = map(int, raw_input().split())
 govs = map(int, raw_input().split())
 graph = {}
@@ -12,13 +13,41 @@ for _ in xrange(m):
         graph[j].append(i)
     else:
         graph[j] = [i]
-st = []
-visited = [False] * n
 
-def dfs():
-    u = st.pop()
-    visited[u - 1] = True
+visited = [0]*n
 
+def dfs(u):
+    """
+        Checks if there not exists a path between "u" and a government
+    """
+    visited[u - 1] = 1
+    #print u
+    flag = True
+    if graph.get(u) != None:
+        for v in graph[u]:
+            if v in govs:
+                return False
+            elif visited[v - 1] == 0:
+                flag = flag and dfs(v)
+
+    return flag
+
+
+for i in xrange(n):
+    node = []
+    if graph.get(i + 1):
+        node = graph[i + 1]
+
+    for j in xrange(n):
+        if i == j or j in node:
+            continue
+        else:
+            if graph.get(i + 1):
+                graph[i + 1].append(j + 1)
+            else:
+                graph[i + 1] = [j + 1]
+
+print dfs(1)
 
 
 print graph
